d60e27a4c6fc8785271a8dbb781f5787b212a09c
David Xu <mysolowing@gmail.com>
2015-12-09 14:12:01 -0600

    added formal bash

---
+++ bash_aliases.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#echo "In aliases.sh"
+
+############################################################
+########## Simpe aliases
+############################################################
+alias c='clear'
+alias s='source ~/.bashrc'
+alias h='history'
+alias j='jobs -l'
+
+############################################################
+########## Functions
+############################################################
+#foo() { ssh "$@" ;}
+foo_ssh() { ssh -i ~/.ssh/id_rsa_trds -x root@"$@";}
+
+
+############################################################
+########## Terminal Only Configuration
+############################################################
+
+alias ls='ls  --color=tty'
+alias la='ls -aChF --color=tty'
+alias lla='ls  -alhF --color=tty'
+alias lls='ls --sort=size -lhFr'
+alias l.='ls -d .* --color=auto'
+
+alias sshpa=foo_ssh
+
+############################################################
+########## Terminal Only Configuration
+############################################################
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+############################################################
+########## Terminal Only Configuration
+############################################################
+alias dua='du --max-depth=0 -ahc * && echo " "'
+alias dus='du --max-depth=0 -kc -a * | sort && echo " " '
+alias duf='du -sk * | sort -n | perl -ne '\''($s,$f)=split(m{\t});for (qw(K M G)) {if($s<1024) {printf("%.1f",$s);print "$_\t$f"; last};$s=$s/1024}'\'
+
+############################################################
+########## Terminal Only Configuration
+############################################################
+alias v='tightvncserver -depth 16 -geometry \!:1x\!:2 '
+alias vv='tightvncserver -depth 16 -geometry 1920x1000'
+alias vk='tightvncserver -kill :\!:1'
+alias vps='find ~/.vnc/ -name "*.pid"'
